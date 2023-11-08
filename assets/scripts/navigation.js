3984564897c899cc8ca2499d429c5862744de75e
SimonGideon <simongideon918@gmail.com>
2023-11-08 11:12:11 +0300

    set-up desktop nav

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const hamburgerBtn = document.querySelector('.humbuger-btn');
     const navLinks = document.querySelector('.plasmic-tab-container');
     const mainBody = document.querySelector('.main-body');
+    const footer = document.querySelector('.footer');
   
     hamburgerBtn.addEventListener('click', () => {
       hamburgerBtn.classList.toggle('active');
       navLinks.classList.toggle('hide');
       mainBody.classList.toggle('hide');
+      footer.classList.toggle('hide');
       if (navContainer) {
         navContainer.classList.toggle('show-nav');
       }
