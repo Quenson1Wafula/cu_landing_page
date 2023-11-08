365c74c85fd1a6ecf398ac844a7c09c9b96b297d
SimonGideon <simongideon918@gmail.com>
2023-11-08 13:14:52 +0300

    refactor Desktop-side menu

--- assets/scripts/navigation.js
+++ assets/scripts/navigation.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const navContainer = document.querySelector('.disapper-nav');
-    const hamburgerBtn = document.querySelector('.humbuger-btn');
-    const navLinks = document.querySelector('.plasmic-tab-container');
-    const mainBody = document.querySelector('.main-body');
-    const footer = document.querySelector('.footer');
-  
-    hamburgerBtn.addEventListener('click', () => {
-      hamburgerBtn.classList.toggle('active');
-      navLinks.classList.toggle('hide');
-      mainBody.classList.toggle('hide');
-      footer.classList.toggle('hide');
-      if (navContainer) {
-        navContainer.classList.toggle('show-nav');
-      }
-    });
+  const navContainer = document.querySelector('.disapper-nav');
+  const hamburgerBtn = document.querySelector('.humbuger-btn');
+  const navLinks = document.querySelector('.plasmic-tab-container');
+  const mainBody = document.querySelector('.main-body');
+  const footer = document.querySelector('.footer');
+  const closeDesktop = document.querySelector('.fa-x');
+  const xContainer = document.querySelector('.x-container');
+
+  hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('active');
+    xContainer.classList.toggle('show');
+    if (navContainer) {
+      navContainer.classList.toggle('show-nav');
+    }
+    // if (!xContainer) {
+    //   xContainer.classList.toggle('show');
+    // }
   });
-  
+
+  closeDesktop.addEventListener('click', () => {
+    hamburgerBtn.classList.remove('active');
+    if (navContainer) {
+      navContainer.classList.toggle('show-nav');
+    }
+  });
+});
+
+
